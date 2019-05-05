3c8a01355c2fdf987335086b7d13aec4261769f4
Biarys <sbiarys@gmail.com>
2019-05-05 18:07:03 -0400

    temp

--- Middleware/hello.py
+++ Middleware/hello.py
@@ -14,8 +14,8 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 @app.route('/')
-def hello_world():
-    return render_template('chart.html')
+def home_page():
+    return render_template('home.html')
 
 
 # @app.route('/charts')
