cff8961d00f611e1e4a2288c6aa883e6d2e04242
Biarys <sbiarys@gmail.com>
2019-05-22 10:58:20 -0400

    connects to db

---
+++ Core/database_stuff.py
@@ -0,0 +1,27 @@
+# from sqlalchemy import create_engine
+
+# engine = create_engine('postgresql://postgres:undead2018@localhost:5432/postgres')
+
+import sqlalchemy
+
+
+def connect(user, password, db, host='localhost', port=5432):
+    '''Returns a connection and a metadata object'''
+    # We connect with the help of the PostgreSQL URL
+    # postgresql://federer:grandestslam@localhost:5432/tennis
+    url = 'postgresql://{}:{}@{}:{}/{}'
+    url = url.format(user, password, host, port, db)
+
+    # The return value of create_engine() is our connection object
+    con = sqlalchemy.create_engine(url, client_encoding='utf8')
+
+    # We then bind the connection to MetaData()
+    meta = sqlalchemy.MetaData(bind=con, reflect=True)
+
+    return con, meta
+
+
+con, meta = connect('undead', 'undead2018', 'tennis')
+
+print(con)
+print(meta)
