5d8779f447135884c76c655f3a66909cbdd39244
Biarys <sbiarys@gmail.com>
2019-05-22 15:57:31 -0400

    database connection ready

--- Core/database_stuff.py
+++ Core/database_stuff.py
@@ -2,7 +2,8 @@
 
 # engine = create_engine('postgresql://postgres:undead2018@localhost:5432/postgres')
 
-import sqlalchemy
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float, ForeignKey, DateTime
+import sqlalchemy_utils as sqlu
 
 
 def connect(user, password, db, host='localhost', port=5432):
@@ -13,15 +14,62 @@ def connect(user, password, db, host='localhost', port=5432):
     url = url.format(user, password, host, port, db)
 
     # The return value of create_engine() is our connection object
-    con = sqlalchemy.create_engine(url, client_encoding='utf8')
+    con = create_engine(url, client_encoding='utf8')
 
-    # We then bind the connection to MetaData()
-    meta = sqlalchemy.MetaData(bind=con, reflect=True)
+    if sqlu.database_exists(url):
+        # We then bind the connection to MetaData()
+        meta = MetaData(bind=con, reflect=True)
+        create_tables(con, meta)
 
-    return con, meta
+        return con, meta
+
+    else:
+        print("============================================")
+        print("Database {} didn't exist".format(db))
+        print("Creating database with schemas")
+        print("============================================")
+        sqlu.create_database(url)
+        print("Database {} and schemas have been created".format(db))
 
 
-con, meta = connect('undead', 'undead2018', 'tennis')
+def create_tables(con, meta):
+    '''Create tables'''
+    backtest = Table("backtest", meta,
+                     Column("backtest_id", Integer, primary_key=True),
+                     Column("name", String(200)))
+    backtest_trades = Table(
+        "backtest_trades", meta,
+        Column("backtest_id", Integer, ForeignKey("backtest.backtest_id")),
+        Column("trade_id", Integer, primary_key=True),
+        Column("ticker", String(20)), Column("direction", String(20)),
+        Column("entry", DateTime), Column("exit", DateTime),
+        Column("pct_change", String(20)), Column("profit", Float(precision=2)),
+        Column("lot_size", Float(precision=8)),
+        Column("position_value", Float(precision=2)),
+        Column("cum_profit", Float(precision=2)), Column("bars_held", Integer))
+
+    # Column("CAGR", Float(precision=2)),
+    # Column("maxDD", Float(precision=2)),
+    # Column("initial_capital", Float(precision=2)),
+    # Column("ending_capital", Float(precision=2)),
+    # Column("net_profit", Float(precision=2)),
+    # Column("net_profit_pct", Float(precision=2)),
+    # Column("transaction_cost", Float(precision=2)),
+    # Column("number_of_trades", Float(precision=2)),
+    # Column("avg_profit", Float(precision=2)),
+    # Column("avg_profit_pct", Float(precision=2)),
+    # Column("avg_bars_held", Float(precision=2)),
+    # Column("net_profit", Float(precision=2)),
+    # Column("net_profit", Float(precision=2)),
+    # Column("net_profit", Float(precision=2)),
+    # Column("net_profit", Float(precision=2)),
+    # Column()
+    # Column()
+
+    meta.create_all(con)
+
+
+con, meta = connect('postgres', 'undead2018', 'tennis2')
 
 print(con)
 print(meta)
