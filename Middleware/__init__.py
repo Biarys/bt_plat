89dc5a4014704e84873bd452d65092401980b885
Biarys <sbiarys@gmail.com>
2019-06-16 20:48:14 -0400

    debug reads from db

--- Middleware/__init__.py
+++ Middleware/__init__.py
@@ -1,11 +1,18 @@
 import os
 from flask import Flask
 from flask import Flask, render_template
-import pandas as pd
 from flask import jsonify
-from contextlib import contextmanager
-import os
+import pandas as pd
+# from contextlib import contextmanager
 import numpy as np
+
+# to import Core
+import sys
+sys.path.insert(0, r"D:\Common\Default\Documents\Projects\bt_plat\Core")
+# print(sys.path)
+import config
+import data_reader
+import database_stuff as db
 
 # app = Flask(__name__)
 # app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -13,6 +20,12 @@ import numpy as np
 # csvpath = os.path.abspath(os.path.join(os.getcwd(), "../stock_data/AAPL.csv"))
 # df = pd.read_csv(csvpath)
 # df["Date"] = pd.to_datetime(df["Date"]).astype(np.int64) / 1000000
+
+con, meta, session = db.connect(config.user, config.password, config.db)
+meta.reflect(bind=con)
+
+data = data_reader.DataReader()
+data.readDB(con, meta, index_col="Date")
 
 
 def create_app(test_config=None):
@@ -44,14 +57,19 @@ def create_app(test_config=None):
     @app.route('/chart', endpoint='chart')
     def chart():
         df = pd.read_csv("stock_data\AA.csv")
-        df = df.to_json(orient="records")
-        # df = df.replace('"', '')
+        df = df.to_dict(orient="records")
         return render_template('chart.html', df=df)
 
     @app.route('/debug', endpoint='debug')
     def debug():
-        df = pd.read_csv("stock_data\AA.csv")
+        # df = pd.read_csv("stock_data\AA.csv")
+        df = data.data["data_AAPL"]
+        # need to reset index, otherwise Date is not sent
+        df.reset_index(inplace=True)
         df = df.to_dict(orient="records")
+
+        
+
         return render_template('debug.html', df=df)
 
     return app
