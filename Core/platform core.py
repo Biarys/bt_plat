0dab01fe4bea10a9f31b3b5a0ac74278b4b49c7e
Biarys <sbiarys@gmail.com>
2019-06-21 22:53:59 -0400

    core looks ready

--- Core/platform core.py
+++ Core/platform core.py
@@ -272,7 +272,7 @@ class Portfolio:
         self.capUsed = pd.DataFrame()
 
 
-Port = Portfolio()
+port = Portfolio()
 ats = Agg_TradeSingal()
 atp = Agg_TransPrice()
 t = Trades()
@@ -359,23 +359,27 @@ def run_portfolio():
     """
     prepricing()
 
+    # copy index and column names for weights
     t.weights = pd.DataFrame(
         index=t.inTradePrice.index, columns=t.inTradePrice.columns)
-    t.priceChange = t.inTradePrice - t.inTradePrice.shift()
+    # t.priceChange = t.inTradePrice - t.inTradePrice.shift()
 
     # Fill with 0s, otherwise results in NaN for port.availAmount
-    t.priceChange.fillna(0, inplace=True)
+    # t.priceChange.fillna(0, inplace=True)
 
-    # calc portfolio change
+    # prepare value, avail amount, invested
+    # copy index and column names for portfolio change
     port.value = pd.DataFrame(
         index=t.inTradePrice.index, columns=["Portfolio value"])
     port.value.iloc[0] = port.startAmount
 
+    # copy index and set column name for avail amount
     port.availAmount = pd.DataFrame(
         index=t.inTradePrice.index, columns=["Available amount"])
     port.availAmount.iloc[0] = port.startAmount
     # port.availAmount.ffill(inplace=True)
 
+    # copy index and column names for invested amount
     port.invested = pd.DataFrame(
         index=t.inTradePrice.index, columns=t.weights.columns)
     port.invested.iloc[0] = 0
@@ -386,7 +390,8 @@ def run_portfolio():
     # set weights to 0 when exit
     # t.weights.loc[atp.sellPrice.index] = 0
 
-    # change change to avoid error
+    # change column names to avoid error
+
     atp.buyPrice.columns = t.weights.columns
     atp.sellPrice.columns = t.weights.columns
 
@@ -397,22 +402,23 @@ def run_portfolio():
     # allocate weights
     for ix, row in t.weights.iterrows():
         # weight = port value / entry
+        # return_prev_bar()
         prev_bar = port.availAmount.index.get_loc(ix) - 1
 
         # not -1 cuz it will replace last value
         if prev_bar != -1:
-            # update avail amount
+            # update avail amount (roll)
             port.availAmount.loc[ix] = port.availAmount.iloc[prev_bar]
 
-            # update invested amount
+            # update invested amount (roll)
             port.invested.loc[ix] = port.invested.iloc[prev_bar]
 
-            # update weight anyway cuz if buy, the wont roll for other stocks
+            # update weight anyway cuz if buy, the wont roll for other stocks (roll)
             t.weights.loc[ix] = t.weights.iloc[prev_bar]
 
         # if there was an entry on that date
         # allocate weight
-        # update avail amount
+        # update avail amount (subtract)
         if ix in atp.buyPrice.index:
             toInvest = port.availAmount.loc[ix, "Available amount"] * 0.1
             stocksAffected = atp.buyPrice.loc[ix].dropna().index.values
@@ -444,9 +450,9 @@ def run_portfolio():
         # update avail amount for gains/losses that day
         # done in the end to avoid factroing it in before buy
         # if != -1 to skip first row
-        if prev_bar != -1:
-            port.availAmount.loc[ix] += (
-                t.priceChange.loc[ix] * t.weights.loc[ix]).sum()
+        # if prev_bar != -1:
+        #     port.availAmount.loc[ix] += (
+        #         t.priceChange.loc[ix] * t.weights.loc[ix]).sum()
 
         # profit = weight * chg
         # portfolio value += profit
