3213d13d16a66d33542da01654f85f45ed5b2d33
Biarys <sbiarys@gmail.com>
2019-10-14 10:22:06 -0400

    main_run runs

--- main_run.py
+++ main_run.py
@@ -1,4 +1,28 @@
 import Core.platform_core as bt
+import Core.Settings as Settings
+from Core.indicators import SMA
 
 if __name__ == "__main__":
-    pass
+    Settings.read_from_csv_path = r"E:\Windows\Documents\bt_plat\stock_data\AA.csv"
+    Settings.read_from = "csvFile"
+    
+    Settings.buy_delay = 1
+    Settings.sell_delay = 1
+
+    class Strategy(bt.Backtest):
+        def logic(self, current_asset):
+            
+            sma5 = SMA(current_asset, ["Close"], 5)
+            sma25 = SMA(current_asset, ["Close"], 25)
+
+            buyCond = sma5() > sma25()
+            sellCond = sma5() < sma25()
+            
+            shortCond = None
+            coverCond = None
+
+            return buyCond, sellCond, shortCond, coverCond
+    
+    s = Strategy("Test_SMA")
+    s.run()
+    s.trade_list.to_csv("test.csv")
