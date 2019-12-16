ce587a21662b9fd7e34ad40eee80f3906dda3b8b
Biarys <sbiarys@gmail.com>
2019-12-15 22:38:10 -0500

    main now submits orders

--- main_run.py
+++ main_run.py
@@ -11,15 +11,25 @@ import threading
 import json
 from datetime import datetime as dt
 
+def submit_orders(bt_orders, current_orders):
+    for ix, order in bt_orders.iterrows():
+        # _buy_or_sell = "BUY" if order["Direction"] == "Long" else "SELL"
+        _asset = order["Symbol"].split(".")
+        _asset = "".join(_asset)
+        app.placeOrder(app.nextOrderId(), at.IBContract.forex(file["forex"][_asset]), at.IBOrder.MarketOrder("BUY", order["Position_value"]))
 
-def run_every_min(strat, data):
+def run_every_min(data):
     prev_min = None
     while True:
         now = dt.now()
         recent_min = now.minute
-        if now.second == 1 and recent_min != prev_min:
+        if now.second == 5 and recent_min != prev_min:
             prev_min = recent_min
+            # TODO: replaced hardcoded Strategy. Gotta find a way to invoke new object each run/appen to previoius one (prob better)
+            strat = Strategy("Test_SMA") # gotta create new object, otherwise it duplicates previous results
             strat.run(data)
+            bt_orders = strat.trade_list[strat.trade_list["Date_exit"] == "Open"]
+            submit_orders(bt_orders, app.open_orders)
             print("Running strategy")
 
 if __name__ == "__main__":
@@ -43,7 +53,7 @@ if __name__ == "__main__":
 
             return buyCond, sellCond, shortCond, coverCond
     
-    s = Strategy("Test_SMA")
+    # s = Strategy("Test_SMA")
     # s.run()
     # s.trade_list = s.trade_list.round(2)
     # s.trade_list.to_csv("trades.csv")
@@ -65,14 +75,14 @@ if __name__ == "__main__":
     # app.placeOrder(app.nextOrderId(), at.IBContract.EurGbpFx(), at.IBOrder.LimitOrder("BUY", 500, 0.85))
     # app.placeOrder(app.nextOrderId(), at.IBContract.EurGbpFx(), at.IBOrder.Stop("BUY", 500, 0.87))
     # print(app.nextValidOrderId)
-    # app.reqOpenOrders()
+    app.reqOpenOrders()
     # app.cancelOrder(10)
     # app.reqPositions()
     app.reqHistoricalData(reqId=app.nextOrderId(), 
                         contract=at.IBContract.forex(file["forex"]["EURGBP"]),
                         #endDateTime="20191125 00:00:00",
                         endDateTime="",
-                        durationStr="1800 S", 
+                        durationStr="1 D", 
                         barSizeSetting="1 min",
                         whatToShow="MIDPOINT",
                         useRTH=1,
@@ -84,14 +94,15 @@ if __name__ == "__main__":
                         contract=at.IBContract.forex(file["forex"]["EURUSD"]),
                         #endDateTime="20191125 00:00:00",
                         endDateTime="",
-                        durationStr="1800 S", 
+                        durationStr="1 D", 
                         barSizeSetting="1 min",
                         whatToShow="MIDPOINT",
                         useRTH=1,
                         formatDate=1,
-                        keepUpToDate=False,
+                        keepUpToDate=True,
                         chartOptions=[]
                         )
     # print(app.data_tracker)
     # print(app.data)
-    run_every_min(s, app.data)
+    # s.run(app.data)
+    run_every_min(app.data)
