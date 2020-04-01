e27ebf194e027adc686b9ff9960c7d8db12c4bcd
Biarys <sbiarys@gmail.com>
2020-04-01 13:38:28 -0400

    added atr to indicators

--- Backtest/indicators.py
+++ Backtest/indicators.py
@@ -34,3 +34,17 @@ class SMA(Indicator):
         self.result.fillna(np.NaN, inplace=True)
         # need to convert dataframe to series for comparison with series
         return pd.Series(self.result["Close"], self.result.index)
+
+class ATR(Indicator):
+    def __init__(self, current_asset, period):
+        self.data = current_asset
+        self.period = period
+
+    def __call__(self):
+        temp = pd.DataFrame()
+        temp["Method1"] = self.data["High"] - self.data["Low"]
+        temp["Method2"] = abs(self.data["High"] - self.data["Close"].shift())
+        temp["Method3"] = abs(self.data["Low"] - self.data["Close"].shift())
+        temp = temp.max(1) # get max of all 3 methods
+        temp = temp.ewm(14).mean()
+        return temp
