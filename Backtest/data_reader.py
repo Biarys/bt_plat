83ad11f0eca56f1f1053388980adafc751df6aa7
Biarys <sbiarys@gmail.com>
2020-05-27 18:42:38 -0400

    commented out db section

--- Backtest/data_reader.py
+++ Backtest/data_reader.py
@@ -1,11 +1,11 @@
 import os
 import pandas as pd
-from functools import wraps
+# from functools import wraps
 
 # own files
-from . import database_stuff as db
-from . import config
-from . import Settings
+# from . import database_stuff as db
+# from . import config
+# from . import Settings
 
 # TODO: add support for csv/db
 class DataReader:
@@ -67,40 +67,40 @@ class DataReader:
         _temp.index = pd.to_datetime(_temp.index)
         return (_fileName, _temp)
 
-    def readDB(self, con, meta, index_col):
-        """
-        Reads tables from database that start with data_.
-        If index_col is not provided, default name "Date" is used for index.
-        Index is converted to pd.to_datetime(), so it's important to provide one.
-        """
-        con, meta = db.connect(config.user, config.password, config.db)
-        meta.reflect(bind=con)
+    # def readDB(self, con, meta, index_col):
+    #     """
+    #     Reads tables from database that start with data_.
+    #     If index_col is not provided, default name "Date" is used for index.
+    #     Index is converted to pd.to_datetime(), so it's important to provide one.
+    #     """
+    #     con, meta = db.connect(config.user, config.password, config.db)
+    #     meta.reflect(bind=con)
 
-        for table in meta.tables.keys():
-            if table.startswith("data_"):
-                _temp = pd.read_sql_table(table, con, index_col=index_col)
-                _temp.index.name = "Date"
-                _temp.index = pd.to_datetime(_temp.index)
-                self.data[table] = _temp
+    #     for table in meta.tables.keys():
+    #         if table.startswith("data_"):
+    #             _temp = pd.read_sql_table(table, con, index_col=index_col)
+    #             _temp.index.name = "Date"
+    #             _temp.index = pd.to_datetime(_temp.index)
+    #             self.data[table] = _temp
 
-        # con.close()
-    # add conditional decorator
-    def establish_con(func):
-        if Settings.read_from.lower()=="db":
-            con, meta = db.connect(config.user, config.password, config.db)
-            meta.reflect(bind=con)
+    #     # con.close()
+    # # add conditional decorator
+    # def establish_con(func):
+    #     if Settings.read_from.lower()=="db":
+    #         con, meta = db.connect(config.user, config.password, config.db)
+    #         meta.reflect(bind=con)
 
-            # @wraps(func)
-            def inner(self, *args, **kwargs):
-                return func(self, con, *args, **kwargs)
+    #         # @wraps(func)
+    #         def inner(self, *args, **kwargs):
+    #             return func(self, con, *args, **kwargs)
 
-            # con.close() # engine closes connection automatically?
-            return inner
+    #         # con.close() # engine closes connection automatically?
+    #         return inner
     
-    @establish_con
-    def execQuery(self, con, query):
-        result = pd.read_sql(query, con)
-        return result
+    # @establish_con
+    # def execQuery(self, con, query):
+    #     result = pd.read_sql(query, con)
+    #     return result
 
     # def read_hdf_pd(self, path):
     #     import h5py
