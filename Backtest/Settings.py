320caeaeb90f131c4949a84b7919543b449adac9
biarys <sbiarys@gmail.com>
2026-02-11 19:06:07 +0500

    refactor Settings class to encapsulate configuration options

--- Backtest/Settings.py
+++ Backtest/Settings.py
@@ -1,50 +1,37 @@
-# run options
-read_from = "csvFiles"
-read_from_csv_path = ""
-log_folder = "LOGS"
-log_name = "test.log"
-save_temp_parquet = "parq/temp"
+class Settings:
+    # run options
+    read_from = "csvFiles"
+    read_from_csv_path = ""
+    log_folder = "LOGS"
+    log_name = "test.log"
+    save_temp_parquet = "parq/temp"
 
-# data_frequency = "D"
+    # simulation options
+    start_amount = 10000
 
-# simulation options
-start_amount = 10000
+    # which col to use for calc buy/sell price
+    buy_on = "Close"
+    sell_on = "Close"
+    short_on = "Close"
+    cover_on = "Close"
 
-# which col to use for calc buy/sell price
-buy_on = "Close"
-sell_on = "Close"
-short_on = "Close"
-cover_on = "Close"
+    # number of bars to delay entry/exit
+    buy_delay = 0
+    sell_delay = 0
+    short_delay = 0
+    cover_delay = 0
 
-# number of bars to delay entry/exit
-buy_delay = 0
-sell_delay = 0
-short_delay = 0
-cover_delay = 0
-# min_bars_hold
-# max_bars_hold or exit_after_bars ?
+    pct_invest = 0.1
+    round_to_decimals = 0
 
-# ? replace with dict?
-pct_invest = 0.1
-round_to_decimals = 0
+    position_size_type = "pct"
+    position_size_value = 0.1 # placeholder, will be used for "share" or "amount"
 
-# not implemented yet
-# max_open_positions = None
-# max_open_long = None
-# max_open_short = None
-# set_margin = 100
+    generate_ranks = False
+    rank_file_name = "rank.csv"
+    order_ranks_desc = False
 
-# # position size
-# min_shares = 0
-# min_position_value = 0
-# max_shares = 0
-# max_position_value = 0
-position_size_type = "pct"
+    path_to_mapping = r"auto_trading/mapping.json"
 
-generate_ranks = False
-rank_file_name = "rank.csv"
-order_ranks_desc = False
-
-path_to_mapping = r"auto_trading/mapping.json"
-
-backtest_engine = "pandas"
+    backtest_engine = "pandas"
+    use_complete_candles_only = True
