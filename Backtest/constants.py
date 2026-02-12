6ca60825c477a1d4eedd5627376741a6d53b9fe0
biarys <sbiarys@gmail.com>
2026-02-12 13:41:50 +0500

    add constants for trade and portfolio management

---
+++ Backtest/constants.py
@@ -0,0 +1,37 @@
+# Column Names
+DATE = "Date"
+OPEN = "Open"
+HIGH = "High"
+LOW = "Low"
+CLOSE = "Close"
+VOLUME = "Volume"
+SYMBOL = "Symbol"
+DATETIME = "DateTime"
+
+# Trade Directions
+LONG = "Long"
+SHORT = "Short"
+
+# Order Signals
+BUY = "Buy"
+SELL = "Sell"
+COVER = "Cover"
+
+# Trade List Columns
+ENTRY_PRICE = "Entry_price"
+EXIT_PRICE = "Exit_price"
+DIRECTION = "Direction"
+DATE_ENTRY = "Date_entry"
+DATE_EXIT = "Date_exit"
+WEIGHT = "Weight"
+DOLLAR_CHANGE = "Dollar_change"
+PCT_CHANGE = "Pct_change"
+DOLLAR_PROFIT = "Dollar_profit"
+PCT_PROFIT = "Pct_profit"
+CUM_PROFIT = "Cum_profit"
+PORTFOLIO_VALUE = "Portfolio_value"
+POSITION_VALUE = "Position_value"
+TRADE_DURATION = "Trade_duration"
+STOP_LOSS = "stop_loss"
+RANK = "rank"
+RANK_DESC = "rank_desc"
